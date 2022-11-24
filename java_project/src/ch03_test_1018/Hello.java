c96322451b70ba63d78109237be0a1be108a819e
jhe2426 <jhe2426@naver.com>
2022-11-24 12:23:19 +0900

    저장1

---
+++ java_project/src/ch03_test_1018/Hello.java
@@ -0,0 +1,15 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 여러줄 주석은 ctrl + shift + /
+		// 여러줄 주석 해제 ctrl + shift + \
+		// 자동 포맷 정렬 : ctrl + shift + f
+		System.out.println("Hello 정하은 자바");
+
+	}
+
+
+}
---
+++ java_project/src/test/Hello.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello 정하은 자바");
+	}
+
+}
